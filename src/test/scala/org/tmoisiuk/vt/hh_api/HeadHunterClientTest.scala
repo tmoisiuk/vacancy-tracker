0be39aaed12b7cf7517a8cc3cae5a1a172f1f341
Taras Moisiuk <taras.moisiuk@kohls.com>
2020-04-09 21:43:15 +0300

    Head Hunter client

---
+++ src/test/scala/org/tmoisiuk/vt/hh_api/HeadHunterClientTest.scala
@@ -0,0 +1,20 @@
+package org.tmoisiuk.vt.hh_api
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class HeadHunterClientTest extends AnyFunSuite {
+
+  test("HH Client should create Search from search config") {
+
+
+
+
+
+
+
+
+
+
+  }
+
+}
